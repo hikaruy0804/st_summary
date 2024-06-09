40c7d6fa615b6c53224d721312c5fb55bba68050
hikaruy0804 <88568970+hikaruy0804@users.noreply.github.com>
2024-06-10 01:08:05 +0900

    Update summary_zoom.py

--- summary_zoom.py
+++ summary_zoom.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import re
+import math
 from janome.analyzer import Analyzer
 from janome.charfilter import UnicodeNormalizeCharFilter, RegexReplaceCharFilter
 from janome.tokenizer import Tokenizer as JanomeTokenizer
@@ -8,24 +9,24 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 
-def normalize_text(contents):
+def start_document_summarize(contents, ratio):
+    """
+    文章を要約する関数
+    :param contents: 要約する文章
+    :param ratio: 要約率（%）
+    """
+    # 文章の正規化と文単位での分割
+    contents = ''.join(contents)
     # 氏名と時間を取り除くための正規表現パターン
     pattern = r"\[.*?\] \d{2}:\d{2}:\d{2}\n"
     # パターンに一致する部分を削除
-    contents = re.sub(pattern, "", contents)
+    contents  = re.sub(pattern, "", contents)
     contents = contents.strip()
-    # 文章を文単位で分割
-    text = re.findall("[^。]+。?", contents.replace('\n', ''))
-    return text
 
-def start_document_summarize(text, ratio):
-    """
-    文章を要約する関数
-    :param text: 要約する文章
-    :param ratio: 要約率（%）
-    """
+    text = re.findall("[^。]+。?", contents.replace('\n', ''))
+    
     # Janomeの設定
-    tokenizer = JanomeTokenizer()
+    tokenizer = JanomeTokenizer('japanese')
     char_filters = [
         UnicodeNormalizeCharFilter(),
         RegexReplaceCharFilter(r'[()「」、。]', ' ')
@@ -42,31 +43,46 @@ def start_document_summarize(text, ratio):
     # Sumyの設定
     parser = PlaintextParser.from_string(''.join(corpus), Tokenizer('japanese'))
     summarizer = LexRankSummarizer()
+    summarizer.stop_words = [' ']
+    
+    # 要約率をセンテンス数に変換
+    lens = len(corpus)
+    a = 100 / lens
+    pers = ratio / a
+    pers = math.ceil(pers)
     
     # 要約の実行
-    summary = summarizer(document=parser.document, sentences_count=ratio)
+    summary = summarizer(document=parser.document, sentences_count=int(pers))
     
     # 要約結果の表示
-    st.write('文書要約完了:')
+    print(u'文書要約完了')
     for sentence in summary:
-        st.write(sentence)
+        st.write(text[corpus.index(sentence.__str__())])
 
 # Webアプリケーションのインターフェース
 st.title("文章要約システム")
 st.write("長文の議事録や資料の文章を要約できます。要約率を入力して「要約開始」ボタンを押してください。")
+st.write("+zoomの文字起こしフォーマット処理を加えています。")
+# 要約率の入力
 ratio = st.number_input(label="要約率 ex:30(%)", min_value=1, max_value=99, value=30, step=1)
 
 # 入力方法の選択
 select_box = st.selectbox("入力方法：", ["直接入力", "テキストファイル(.txt)"])
+
 contents = ""
+
+# 直接入力またはファイルアップロードに応じてコンテンツを取得
 if select_box == "直接入力":
-    contents = st.text_area(label="入力欄", height=500)
+    texts = st.text_area(label="入力欄", height=500)
+    contents = texts.lower()
 elif select_box == "テキストファイル(.txt)":
     uploaded_file = st.file_uploader(label='Upload file:')
+    st.write('input: ', uploaded_file)
     if uploaded_file is not None:
-        contents = uploaded_file.getvalue().decode('utf-8')
+        texts = uploaded_file.getvalue()
+        texts = texts.decode('utf-8')
+        contents = texts.lower()
 
 # 「要約開始」ボタンが押された場合の処理
 if st.button("要約開始") and contents:
-    text = normalize_text(contents)
-    start_document_summarize(text, ratio)
+    start_document_summarize(contents, ratio)
