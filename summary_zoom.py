8b2f2cf51ed86f139f0325d245d0725f9799ca67
hikaruy0804 <88568970+hikaruy0804@users.noreply.github.com>
2024-06-10 00:56:11 +0900

    Update summary_zoom.py

--- summary_zoom.py
+++ summary_zoom.py
@@ -17,13 +17,13 @@ def start_document_summarize(contents, ratio):
     """
     # 文章の正規化と文単位での分割
     contents = ''.join(contents)
+    text = re.findall("[^。]+。?", contents.replace('\n', ''))
+    
     # 氏名と時間を取り除くための正規表現パターン
     pattern = r"\[.*?\] \d{2}:\d{2}:\d{2}\n"
     # パターンに一致する部分を削除
-    contents  = re.sub(pattern, "", contents)
-    contents = contents.strip()
-
-    text = re.findall("[^。]+。?", contents.replace('\n', ''))
+    text = re.sub(pattern, "", text)
+    text = contents.strip()
     
     # Janomeの設定
     tokenizer = JanomeTokenizer('japanese')
@@ -73,15 +73,15 @@ contents = ""
 
 # 直接入力またはファイルアップロードに応じてコンテンツを取得
 if select_box == "直接入力":
-    texts = st.text_area(label="入力欄", height=500)
-    contents = texts.lower()
+     contents = st.text_area(label="入力欄", height=500)
+    # contents = texts.lower()
 elif select_box == "テキストファイル(.txt)":
     uploaded_file = st.file_uploader(label='Upload file:')
     st.write('input: ', uploaded_file)
     if uploaded_file is not None:
         texts = uploaded_file.getvalue()
-        texts = texts.decode('utf-8')
-        contents = texts.lower()
+         contents = texts.decode('utf-8')
+        # contents = texts.lower()
 
 # 「要約開始」ボタンが押された場合の処理
 if st.button("要約開始") and contents:
